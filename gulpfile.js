0c60d33a5be902964b33046163fb5356cfe84e6d
chujunfang <chujunfang@qfpay.com>
2017-06-09 14:09:31 +0800

    base64图片

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,37 @@
 
 // 引入 gulp及组件
 var gulp = require('gulp'), //基础库
-    imagemin = require('gulp-imagemin'), //图片压缩
+    imagemin = require('gulp-imagemin'), //图片压缩    
+    spriter = require('gulp-css-spriter'),//控制图片大小自适应显示问题——background-position: 需要变成显示的一半，background-size:200% auto;---取消雪碧图
+    spritesmith = require('gulp.spritesmith'), //图片精灵,取消使用
+    imageResize = require('gulp-image-resize'), //取消使用--报错
+    pngquant = require('imagemin-pngquant'), //取消使用
+    buffer = require('vinyl-buffer'),//取消使用
+    csso = require('gulp-csso'),//取消使用
+    merge = require('merge-stream'),//取消使用
+
     less = require('gulp-less'), //less
     minifycss = require('gulp-minify-css'), //css压缩
     autoprefixer = require('gulp-autoprefixer'), //使用gulp-autoprefixer根据设置浏览器版本自动处理浏览器前缀
     postcss = require('gulp-postcss'), //单位转化px--rem
     px2rem = require('postcss-px2rem'), //单位转化px--rem
+    base64 = require('gulp-base64'),//图片转base64编码
+
     jshint = require('gulp-jshint'), //js检查
-    jscs=require('gulp-jscs'),
+    jscs = require('gulp-jscs'),
     stylish = require('jshint-stylish'),
     uglify = require('gulp-uglify'), //js压缩
+
     rename = require('gulp-rename'), //重命名
     concat = require('gulp-concat'), //合并文件
     clean = require('gulp-clean'), //清空文件夹--同del，本例取消clean
     del = require('del'), //删除文件
     header = require('gulp-header'), //给文件头部增加特殊内容
     replace = require('gulp-replace'), //替换str
+
     path = require('path'),
     runSequence = require('run-sequence'), //按顺序执行
+
     rev = require('gulp-rev'), //- 对文件名加MD5后缀
     revCollector = require('gulp-rev-collector'), //- 路径html替换
     connect = require('gulp-connect'), //搭建服务器并自动更新更改--本文件没用使用，而是用的browser-sync
@@ -32,9 +45,12 @@ var gulp = require('gulp'), //基础库
 var file_road = {
     cssSrc: './src/less/**/*.less',
     cssDst: './static/css',
+    imgDst_sprite: './static/sprite/spritesheet.png',
+    imgDst_sprite_css:'../sprite/spritesheet.png',
 
     imgSrc: './src/img/**/*',
     imgDst: './static/img',
+    imgSrc_sprite: './src/img/**/*.png',
 
     jsLocal: './src/js/**/*.js',
     jsLocal_es6_no: ['./src/js/{plug,common}/**/*.js', './src/js/require.2.1.11.min.js', './src/js/require-config.js'],
@@ -48,7 +64,7 @@ var file_road = {
     htmlSrc: './html/**/*.html',
     // htmlDst_mid: './static_dest/template', //测试
     // htmlDst: '../bin/template',
-    htmlSrc_test:'./svg/**/*.html',//测试文件——仅供html实时更新
+    htmlSrc_test: './svg/**/*.html', //测试文件——仅供html实时更新
 
     w_cssSrc: 'src/less/**/*.less',
     w_imgSrc: 'src/img/**/*',
@@ -96,6 +112,13 @@ gulp.task('css', function() {
             remove: true //是否去掉不必要的前缀 默认：true 
         }))
         .pipe(postcss(processors)) //--变得有点小
+        .pipe(base64({
+            baseDir: './static/img',
+            extensions: ['svg', 'png', /\.jpg#datauri$/i],
+            exclude:    [/\.server\.(com|net)\/dynamic\//, '--live.jpg'],
+            maxImageSize: 10*1024, // bytes 
+            debug: true
+        }))
         //.pipe(header(banner, { pkg: pkg })) //增加头部注释
         .pipe(gulp.dest(file_road.cssDst)) //本地目录
         // .pipe(rev()) //版本号
@@ -121,7 +144,11 @@ gulp.task('jshint', function() {
 //js--转es6
 gulp.task('js_local_es6', function() {
     gulp.src(file_road.jsLocal_es6)
-        .pipe(jshint.reporter('default')) //代码检测
+        .pipe(jshint({
+            "undef": true,
+            "unused": false
+        }))
+        .pipe(jshint.reporter(stylish)) //代码检测
         .pipe(babel({
             presets: ['es2015'],
             // modules: "amd" // 默认是 common，也可以改成 umd
@@ -147,6 +174,71 @@ gulp.task('images', function() {
         }))
         .pipe(gulp.dest(file_road.imgDst)) //本地目录
         .pipe(browserSync.stream());
+});
+//测试css精灵图，不使用——控制图片大小自适应显示问题——background-position: 需要变成显示的一半，background-size:200% auto;---取消雪碧图
+gulp.task('sprite', function() {
+    // gulp.src(file_road.imgSrc_sprite)
+    //     // .pipe(imageResize({
+    //     //     percentage: 10
+    //     // }))
+    //     // .pipe(imagemin({
+    //     //     optimizationLevel: 7, //类型：Number  默认：3  取值范围：0-7（优化等级）
+    //     //     progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+    //     //     interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+    //     //     multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
+    //     //     use: [pngquant({ quality: '35-60' })]
+    //     // }))
+    //     .pipe(spritesmith({
+    //         imgName: 'sprite.png',
+    //         cssName: 'sprite.css'
+    //     }))
+    //     .pipe(gulp.dest(file_road.imgDst_sprite));
+    // var spriteData = gulp.src(file_road.imgSrc_sprite).pipe(spritesmith({
+    //     imgName: 'sprite.png',
+    //     cssName: 'sprite.css'
+    // }));
+    // return spriteData.pipe(gulp.dest(file_road.imgDst_sprite));
+    // Generate our spritesheet 
+    var spriteData = gulp.src(file_road.imgSrc_sprite).pipe(spritesmith({
+        imgName: 'sprite1.png',
+        cssName: 'sprite1.css',
+        padding: 20
+    }));
+
+    // Pipe image stream through image optimizer and onto disk 
+    var imgStream = spriteData.img
+        // DEV: We must buffer our stream into a Buffer for `imagemin` 
+        .pipe(buffer())
+        .pipe(imagemin())
+        .pipe(gulp.dest(file_road.imgDst_sprite));
+
+    // Pipe CSS stream through CSS optimizer and onto disk 
+    var cssStream = spriteData.css
+        .pipe(csso())
+        .pipe(gulp.dest(file_road.imgDst_sprite));
+
+    // Return a merged stream to handle both `end` events 
+    return merge(imgStream, cssStream);
+});//替换css的问题还没有处理
+gulp.task('css_sprite', function() {
+    var processors = [px2rem({ remUnit: 37.5 })];
+    gulp.src(file_road.cssSrc)
+        .pipe(less({ style: 'expanded' }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'Android >= 4.0', 'last 2 Explorer versions', 'last 3 Safari versions', 'Firefox >= 20', '> 5%'],
+            cascade: true, //是否美化属性值 默认：true 像这样：//-webkit-transform: rotate(45deg);transform: rotate(45deg);
+            remove: true //是否去掉不必要的前缀 默认：true 
+        }))
+        .pipe(spriter({
+            // The path and file name of where we will save the sprite sheet 
+            'spriteSheet': file_road.imgDst_sprite,
+            // Because we don't know where you will end up saving the CSS file at this point in the pipe, 
+            // we need a litle help identifying where it will be. 
+            'pathToSpriteSheetFromCSS': file_road.imgDst_sprite_css
+        }))
+        .pipe(postcss(processors)) //--变得有点小
+        .pipe(gulp.dest(file_road.cssDst)) //本地目录
+
 });
 // 清空图片、样式、js---最终使用del------------------------------------------------------------------------------------------------------------------------------------------
 gulp.task('del', function(cb) {
@@ -242,7 +334,7 @@ gulp.task('watch', function() {
     gulp.watch(file_road.w_jsLocalSrc_es6_no, ['jsLocal_es6_no']);
     // 监听html变动
     gulp.watch(file_road.w_htmlSrc, ['html_fresh']);
-    gulp.watch(file_road.test_html_fresh,['test_html_fresh']);
+    gulp.watch(file_road.test_html_fresh, ['test_html_fresh']);
     // gulp.watch(file_road.w_jsSrc, ['js']);
     // gulp.watch(file_road.w_jsrecon_Src, ['reqconjs']);
     // gulp.watch(file_road.w_jsnum_Src, ['jsnum']);
